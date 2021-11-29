80dd60187e23c6ed488412231207ace5c4346fa9
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-29 15:35:19 +0100

    Created SPA with empty pages

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="all-app">
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "app",
+  components: {},
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "./src/styles/variables";
+body {
+  background-color: $backgroundColor;
+  font-family: "Source Sans Pro", sans-serif, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: $textColor;
 }
-
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: $textColor;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <h1 class="login-title">Coming soon</h1>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped lang="scss">
+@import "./src/styles/variables";
+.login-title {
+  color: $mainColor;
+  text-align: center;
+}
+</style>
