57b8e28769d4d36f47ff0d62a36644f5d3d11ac0
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-29 16:39:18 +0100

    Added the footer HTML

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer-div">
+    <div class="left-footer-container">
+      <ul class="left-footer-list">
+        <li class="about-us">
+          <a href="https://github.com/SkylabCoders/David-Galvez_Front-Final-Project-202109"
+            >About us
+          </a>
+        </li>
+        <li class="help">
+          <a href="https://github.com/SkylabCoders/David-Galvez_Front-Final-Project-202109">Help</a>
+        </li>
+        <li class="others">
+          <a href="https://github.com/SkylabCoders/David-Galvez_Front-Final-Project-202109"
+            >Others</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="right-footer-container">
+      <div class="contact-us-container">
+        <p class="contact-us">Contact us:</p>
+      </div>
+      <div class="social-webs-container">
+        <ul class="social-web-list">
+          <li class="instagram-list-item">
+            <a href="https://www.instagram.com/"><img src="instagram.png" height="55" /></a>
+          </li>
+          <li class="twitter-list-item">
+            <a href="https://twitter.com/"><img src="twitter.png" height="40" /></a>
+          </li>
+          <li class="youtube-list-item">
+            <a href="https://www.youtube.com/"><img src="youtube.png" height="40" /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="bottom-footer-container">
+    <h3 class="footer-copyright">2021 All rights reserved - VueGComps</h3>
+  </div>
+</template>
