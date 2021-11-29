d6628070ec96f105ef4ee24074e86a1b8b9e49a4
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-29 16:55:22 +0100

    Added style to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -64,3 +64,63 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped lang="scss">
+@import "./src/styles/variables";
+.header-nav-bar {
+  margin: 0 20px;
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 85px;
+  width: 88vw;
+}
+.extended-nav-bar {
+  margin: 0 20px;
+  height: 120px;
+  width: 88vw;
+  z-index: 1;
+  margin-bottom: 20px;
+}
+.extended-home,
+.extended-cart,
+.extended-login {
+  background-color: $backgroundColor;
+  height: 40px;
+  padding: 0px;
+  margin: 0px;
+  border-bottom: solid $mainColor 2px;
+}
+.extended-home {
+  border-top: solid $mainColor 2px;
+}
+.extended-home-bar,
+.extended-cart-bar,
+.extended-login-bar {
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  background-color: $backgroundColor;
+  height: 40px;
+  padding: 0px;
+  margin: 0px;
+  &:hover {
+    cursor: pointer;
+    color: $mainColor;
+  }
+}
+.burger-menu-logo,
+.vuegcomp-logo,
+.shopping-cart-logo,
+.login-logo {
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+}
+a {
+  text-decoration: none;
+  color: $textColor;
+}
+</style>
