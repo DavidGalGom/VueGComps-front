c1ed78b841f57e17afa3b528f3ba1ebba9ef18dc
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-29 22:18:53 +0100

    Added css to details page

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -8,16 +8,18 @@
         <p class="component-price">99 €</p>
       </div>
       <div class="container-carousel">
-        <img class="previous-carousel" src="" width="20" height="30" />
+        <img class="previous-carousel" src="previous-f92.png" width="20" height="30" />
         <img
           class="component-image"
           src="https://www.profesionalreview.com/wp-content/uploads/2020/08/AMD-A520-es-lanzado-nuevas-placas-base-de-gama-baja-para-Ryzen_4.jpg"
           alt="Component"
           width="250"
         />
-        <img class="next-carousel" src="" width="20" height="30" />
+        <img class="next-carousel" src="next-f92.png" width="20" height="30" />
       </div>
-      <button class="add-cart-button">Add to cart</button>
+      <div class="button-container">
+        <button class="add-cart-button">Add to cart</button>
+      </div>
       <h2 class="details-subtitle">Product details:</h2>
       <p class="details-paragraph">
         qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
@@ -45,5 +47,68 @@
   min-height: 400px;
   background-color: $alterColor;
   border-radius: 25px;
+}
+.back-button {
+  margin-left: 25px;
+  margin-top: 20px;
+  &:hover {
+    cursor: pointer;
+    transform: scale(0.9);
+  }
+}
+.container-name-price {
+  margin: 0 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.component-name {
+  font-size: 18px;
+}
+.component-price {
+  color: $mainColor;
+  font-size: 22px;
+}
+.container-carousel {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.previous-carousel,
+.next-carousel {
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+}
+.add-cart-button {
+  margin: 20px;
+  height: 35px;
+  width: 200px;
+  border-radius: 30px;
+  background-color: $mainColor;
+  color: $alterColor;
+  font-size: 24px;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    color: $mainColor;
+    background-color: $alterColor;
+    border: solid 2px $mainColor;
+  }
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+.details-subtitle {
+  font-size: 18px;
+  margin: 30px;
+}
+.details-paragraph {
+  font-size: 16px;
+  padding: 0 30px;
+  word-break: break-all;
+  margin-bottom: 30px;
 }
 </style>
