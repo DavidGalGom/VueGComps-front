2e65949c47f79ee2d5cf3e5914d27688278c81b9
David Galvez <davidgalvezgomez01@gmail.com>
2021-12-01 21:26:49 +0100

    Added test for Product component

---
+++ tests/unit/Product.spec.ts
@@ -0,0 +1,27 @@
+import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
+import router from "../../src/router";
+import state from "../stateMock";
+import Product from "../../src/components/Product.vue";
+import "@testing-library/jest-dom";
+
+describe("Given a Product component", () => {
+  describe("When its rendered", () => {
+    test("Then it should render the div with the card", async () => {
+      const store = createStore({
+        state() {
+          return state;
+        },
+      });
+
+      const wrapper = mount(Product, {
+        global: {
+          plugins: [router, store],
+        },
+        stubs: ["router-view"],
+      });
+
+      expect(wrapper.html()).toContain('<div class="card-container">');
+    });
+  });
+});
