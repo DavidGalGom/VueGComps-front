e2ed236dcf8cc9d328fda1446e3aa7f7f0759c8a
David Galvez <davidgalvezgomez01@gmail.com>
2021-12-01 21:15:08 +0100

    Added test for RegisterForm component

---
+++ tests/unit/RegisterForm.spec.ts
@@ -0,0 +1,29 @@
+import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
+import router from "../../src/router";
+import state from "../stateMock";
+import RegisterForm from "../../src/components/RegisterForm.vue";
+import "@testing-library/jest-dom";
+
+describe("Given a RegisterForm component", () => {
+  describe("When its rendered", () => {
+    test("Then it should render a form", async () => {
+      const store = createStore({
+        state() {
+          return state;
+        },
+      });
+
+      const wrapper = mount(RegisterForm, {
+        global: {
+          plugins: [router, store],
+        },
+        stubs: ["router-view"],
+      });
+
+      expect(wrapper.html()).toContain(
+        '<form class="register-form" autocomplete="off">'
+      );
+    });
+  });
+});
