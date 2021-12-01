3a25b45ebc99d73e482d7b3c3fec7dcf8207ab11
David Galvez <davidgalvezgomez01@gmail.com>
2021-12-01 18:28:55 +0100

    Added test-utils.ts file

---
+++ tests/test-utils.ts
@@ -0,0 +1,13 @@
+import { Commit } from "vuex";
+import { State } from "@/types/interfaces";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const configActionContext = (commit: Commit): any => ({
+  commit,
+  dispatch: jest.fn(),
+  getters: jest.fn(),
+  rootState: {} as State,
+  rootGetters: jest.fn(),
+});
+
+export default configActionContext;
