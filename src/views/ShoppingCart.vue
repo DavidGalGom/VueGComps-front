ea29d41d36da711ae1ed90a28400c8d2bc1481c0
David Galvez <davidgalvezgomez01@gmail.com>
2021-12-03 12:37:11 +0100

    Added html and css of component cartCard

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="shopping-cart">
-    <h1 class="shopping-cart-title">Coming soon</h1>
+    <h1 class="shopping-cart-title">This is your list of products</h1>
+    <CartCard />
+    <CartCard />
+    <CartCard />
   </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import CartCard from "@/components/CartCard.vue";
+
+export default defineComponent({
+  name: "ShoppingCart",
+  components: {
+    CartCard,
+  },
+});
+</script>
 
 <style scoped lang="scss">
 @import "./src/styles/variables";
 .shopping-cart-title {
-  color: $mainColor;
+  color: $textColor;
   text-align: center;
+  font-size: 22px;
 }
 </style>
