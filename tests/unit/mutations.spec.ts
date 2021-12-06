ff7eb23444528cd124d91c1e611be7c3bb2afba5
David Galvez <davidgalvezgomez01@gmail.com>
2021-12-06 18:00:24 +0100

    Added tests for loginUser mutation

--- tests/unit/mutations.spec.ts
+++ tests/unit/mutations.spec.ts
@@ -1,6 +1,6 @@
 import mutations from "@/store/mutations";
 import stateMock from "../stateMock";
-import { Product } from "@/types/interfaces";
+import { Product, User } from "@/types/interfaces";
 
 describe("Given a store mutations object", () => {
   describe("When getProducts receives a state and payload with components", () => {
@@ -44,6 +44,46 @@ describe("Given a store mutations object", () => {
       mutations.getProductById(state, payload);
 
       expect(state.productById).toBe(payload);
+    });
+  });
+
+  describe("When loginUser receives a state and payload with a user", () => {
+    test("Then it should put isAuthenticated = true", () => {
+      const state = stateMock;
+      const payload: User = {
+        name: "user",
+        userName: "user name",
+        password: "password",
+        email: "email",
+        age: 10,
+        isAdmin: false,
+        components: [],
+        image: "image",
+      };
+
+      mutations.loginUser(state, payload);
+
+      expect(state.isAuthenticated).toBe(true);
+    });
+  });
+
+  describe("When loginUser receives a state and payload with a user", () => {
+    test("Then it should put user data in the stage", () => {
+      const state = stateMock;
+      const payload: User = {
+        name: "user",
+        userName: "user name",
+        password: "password",
+        email: "email",
+        age: 10,
+        isAdmin: false,
+        components: [],
+        image: "image",
+      };
+
+      mutations.loginUser(state, payload);
+
+      expect(state.user).toBe(payload);
     });
   });
 });
