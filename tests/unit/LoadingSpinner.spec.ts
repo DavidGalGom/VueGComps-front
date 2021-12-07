015499a48ec620228d1f093b4eb97388125cff5c
David Galvez <davidgalvezgomez01@gmail.com>
2021-12-07 13:09:39 +0100

    Added test for LoadingSpinner componnet

--- tests/unit/LoadingSpinner.spec.ts
+++ tests/unit/LoadingSpinner.spec.ts
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/vue";
-import NotFound from "../../src/views/NotFound.vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
 import "@testing-library/jest-dom";
 
 describe("Given a NotFound component", () => {
   describe("When its rendered ", () => {
     test("Then it should render heading tag", async () => {
-      render(NotFound);
-      const notFoundTitle = await screen.getByRole("heading", {
-        name: "404 Page Not found, please go to a correct route",
+      render(LoadingSpinner);
+      const spinnerTitle = await screen.getByRole("heading", {
+        name: "Loading",
       });
 
-      expect(notFoundTitle).toBeInTheDocument();
+      expect(spinnerTitle).toBeInTheDocument();
     });
   });
 });
